75b71361d6d5154d2c9a32ff65cc52f253ab6a2a
kkdd0757 <kkdd0757@hanmail.net>
2021-07-23 17:45:04 +0900

    feat: create and add function
    Accidently created file in wrong directory.
    So moved this file to fibonachicken directory and updated text.
    
    Function : this function is for controlling input.
    The user should put the number of ppl they should buy chicken.
    
    If the user put below 0, then they will get the text which is pretty funny (i guess).
    However, the result (how many chicken they should buy) can be shown when the user put above 0.

---
+++ fiboChicken.py
@@ -0,0 +1,7 @@
+chicken = input('자애로운자여, 치킨을 몇 명이나 먹이려고 하는가?')
+	
+def input(chicken):
+    if (chicken >0):
+        print(fiboChicken(chicken))
+    else:
+        print('이건 무슨 해괴한말인고. 치킨을 뺏기라도 하겠다는게냐?')
